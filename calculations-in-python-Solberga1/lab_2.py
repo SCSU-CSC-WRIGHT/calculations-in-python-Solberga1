adb9e604f29fca455b218f4cd0c3d2d8adcd9521
Solberga1 <solberga1@southernct.edu>
2024-09-23 16:28:25 -0400

    Add files via upload
    Finished Lab ##2

---
+++ calculations-in-python-Solberga1/lab_2.py
@@ -0,0 +1,9 @@
+### Lab 2 ###
+
+weight= int(input("Enter your weight (kg): "))
+height= int(input("Enter your height (m^2): "))
+
+bmi= weight/height
+
+print(f"Your bmi is: {bmi} ")
+print("You are within a moderate BMI range")
---
+++ calculations-in-python-Solberga1/lab_3.py
@@ -0,0 +1,7 @@
+### Lab 3 ###
+
+celcius=  int(input("Enter the temperature in celcius: "))
+
+fahrenheit= int((celcius * 9/5+ 32))
+
+print(f"The temperture in Fahrenheit is: {fahrenheit} ")
